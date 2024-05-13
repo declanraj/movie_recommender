fd7990feab251214e06cc24162ea79766f362cda
mukundrajrs <mr7149_nyu_edu@inst-builder-debian-10-build-build-r336j.us-central1-a.c.gce-image-builder.internal>
2024-05-13 03:13:06 +0000

    q1q2 completed

--- q1q2.py
+++ q1q2.py
@@ -18,16 +18,11 @@ def main(spark, userID):
     '''
 
     # Load the boats.txt and sailors.json data into DataFrame
-    ratings = spark.read.csv(f'hdfs:/user/{userID}/ml-latest-small/ratings.csv', header = True, schema='userId string, movieId string, rating FLOAT, timestamp INT')
+    ratings = spark.read.csv(f'hdfs:/user/{userID}/ml-latest-small/ratings.csv', header = True, schema='userId INT, movieId string, rating FLOAT, timestamp INT')
     # movies = spark.read.csv(f'hdfs:/user/{userID}/ml-latest-small/movies.csv')
     # links = spark.read.csv(f'hdfs:/user/{userID}/ml-latest-small/links.csv')
     # tags = spark.read.csv(f'hdfs:/user/{userID}/ml-latest-small/tags.csv')
-
-    # tr, val, tst = train_val_test_split(ratings)
-    # tr.orderBy('userId').show()
-    # val.orderBy('userId').show()
-    # tst.orderBy('userId').show()
-
+    
     print('Printing ratings with specified schema')
     ratings.printSchema()
     user_movies = ratings.groupBy("userId").agg(collect_list("movieId").alias("movies"))
@@ -92,59 +87,6 @@ def main(spark, userID):
 
     print("Average Correlation (Movie Twins):", average_correlation)
     print("Average Correlation (Random Pairs):", average_random_correlation)
-
-
-# def train_val_test_split(df):
-    filtered_unique_user = df.groupBy('userId').agg({'rating': 'count'}).withColumnRenamed('count(rating)', 'rating_count')
-    filtered_unique_user = filtered_unique_user.filter(col('rating_count')>=10).select('userId')
-
-    #     train_users = filtered_unique_user.sample(withReplacement=False, fraction=0.7, seed=2024)
-    #     val_test_users = filtered_unique_user.select('userId').subtract(train_users.select('userId'))
-    filtered_unique_user = filtered_unique_user.withColumn('rand', rand(seed=2024))
-    filtered_unique_user = filtered_unique_user.orderBy('rand')
-    filtered_unique_user = filtered_unique_user.drop(col('rand'))
-
-    split_point = int(filtered_unique_user.count() * 0.7)
-
-    train_users = filtered_unique_user.limit(split_point)
-    val_test_users = filtered_unique_user.subtract(train_users)
-
-    #     val_users = val_test_users.sample(withReplacement=False, fraction=0.5, seed=2024)
-    #     test_users = val_test_users.select('userId').subtract(val_users.select('userId'))
-
-    val_test_users = val_test_users.withColumn('rand', rand(seed=2024))
-    val_test_users = val_test_users.orderBy('rand')
-    val_test_users = val_test_users.drop(col('rand'))
-
-    split_point = int(val_test_users.count() * 0.5)
-
-    val_users = val_test_users.limit(split_point)
-    test_users = val_test_users.subtract(val_users)
-
-    print(train_users.count())
-    print(val_test_users.count())
-    print(val_users.count())
-    print(test_users.count())
-
-    train_ratings = df.join(train_users, on='userId', how='inner')
-    val_ratings = df.join(val_users, on='userId', how='inner')
-    test_ratings = df.join(test_users, on='userId', how='inner')
-
-    val_users_list = [row["userId"] for row in val_users.collect()]
-    test_users_list = [row["userId"] for row in test_users.collect()]
-
-    val_fractions = dict(zip(val_users_list, [0.5 for _ in range(len(val_users_list))]))
-    test_fractions = dict(zip(test_users_list, [0.5 for _ in range(len(test_users_list))]))
-        
-    val_train = val_ratings.sampleBy('userId', fractions=val_fractions, seed=2024)
-    validation = val_ratings.subtract(val_train)
-
-    test_train = test_ratings.sampleBy('userId', fractions=test_fractions, seed=2024)
-    test = test_ratings.subtract(test_train)
-
-    train = train_ratings.union(val_train).union(test_train)
-
-    return train, validation, test
 
 
 if __name__ == "__main__":
